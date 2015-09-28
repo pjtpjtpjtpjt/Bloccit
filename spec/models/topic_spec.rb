1a598fada74ea22117256409510e9e2981225ffc
pjtpjtpjtpjt <paul.jo.thompson@gmail.com>
2015-09-28 16:33:15 +0000

    validations

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -8,6 +8,12 @@ let(:topic) { Topic.create!(name: RandomData.random_sentence, description: Rando
 it { should have_many(:posts) }
  
 
+
+   it { should validate_length_of(:name).is_at_least(5) }
+   it { should validate_length_of(:description).is_at_least(15) }
+
+
+
    describe "attributes" do
      it "should respond to name" do
        expect(topic).to respond_to(:name)
