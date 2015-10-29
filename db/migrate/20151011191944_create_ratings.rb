77474d2e141942db4959ed55181ad4ef8c9d764c
pjtpjtpjtpjt <paul.jo.thompson@gmail.com>
2015-10-29 14:58:52 +0000

    change enum field to integer

--- db/migrate/20151011191944_create_ratings.rb
+++ db/migrate/20151011191944_create_ratings.rb
@@ -1,7 +1,7 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
-       t.string :name
+       t.integer :name
        t.references :ratable, polymorphic: true, index: true
  
        t.timestamps null: false
